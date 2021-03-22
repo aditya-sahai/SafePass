65ac69b83e01483c8e7e4491d0983a26f8cb1331
Buddy <adityasahai925@gmail.com>
2021-03-22 13:32:13 +0530

    Fixing Bug In Displaying PFP

--- SafePass/urls.py
+++ SafePass/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from users.views import index
-# from . import settings
+from . import settings
+
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
     path('user/', include("users.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
